ef2075c52b872f6d59502fffe93e17d2f28f36af
Voe <mr.subiter@mail.ru>
2023-09-17 19:26:41 +0500

    feat: Add private fields, properties, constructor

--- Models/Schedule.cs
+++ Models/Schedule.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Shelter.Models
 {
-	internal class Schedule
+	public  class Schedule
 	{
-	}
+		private DateTime year;
+		private DateTime month;
+		private string municipality;
+		// Предполагается, что свойства будут реализовывать валидацию
+		public DateTime Year { get => year; set => year = value; }
+		public DateTime Month { get => month; set => month = value; }
+		public string  Municipality { get => municipality; set => municipality = value; }
+		//TODO: 
+		// Сделать реализацию
+        public Schedule()
+        {
+            
+        }
+    }
 }
