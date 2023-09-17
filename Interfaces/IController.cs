07284f8652f758efe2fda4f4fd8aacdc74a27c9a
Voe <mr.subiter@mail.ru>
2023-09-17 18:05:01 +0500

    feat: Add IController interface

---
+++ Interfaces/IController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shelter.Interfaces
+{
+	public interface IController<T>
+	{
+		List<T> GetCards();
+		T GetCard(int id);
+		void AddCard(T record);
+		void RemoveCard(int id);
+	}
+}
