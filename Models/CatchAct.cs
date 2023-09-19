3b4ec15e6b4b2c2966823acc92c9eda212308df5
Dmitriz1 <stud0000264980@study.utmn.ru>
2023-09-19 14:28:47 +0500

    Classes being redacted

--- Models/CatchAct.cs
+++ Models/CatchAct.cs
@@ -19,6 +19,15 @@ namespace Shelter.Classes
         public int applicid; // applicid = Application ID это номер заявки на отлов
         public int mcid; // mcid = municipal contract id номер карточки муниципального контракта
 
+        public int Caid { get => caid; set => caid = value; }
+        public int Dogcount { get => dogcount; set => dogcount = value; }
+        public int Catcount { get => catcount; set => catcount = value; }
+        public int Animalcount { get => animalcount; set => animalcount = value; }
+        public string Orgname { get => orgname; set => orgname = value; }
+        public DateTime CatchDate { get => catchDate; set => catchDate = value; }
+        public string CatchPurp { get => catchPurp; set => catchPurp = value; }
+        public int Applicid { get => applicid; set => applicid = value; }
+        public int Mcid { get => mcid; set => mcid = value; }
     }
 }
 
--- Models/Organization.cs
+++ Models/Organization.cs
@@ -15,5 +15,12 @@ namespace Shelter.Classes
         public string adress; // адрес организации
         public string orgtype; // тип организации
         public string ipyyr; // ип или юр лицо
+
+        public string Orgname { get => orgname; set => orgname = value; }
+        public int Inn { get => inn; set => inn = value; }
+        public int Kpp { get => kpp; set => kpp = value; }
+        public string Adress { get => adress; set => adress = value; }
+        public string Orgtype { get => orgtype; set => orgtype = value; }
+        public string Ipyyr { get => ipyyr; set => ipyyr = value; }
     }
 }
