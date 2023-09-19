0de7667500ff18847524bf9806de3307ee3b368c
Dmitriz1 <stud0000264980@study.utmn.ru>
2023-09-19 14:19:16 +0500

    Added new classes

---
+++ Models/CatchAct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shelter.Classes
+{
+    public class CatchAct
+    {
+        public int caid; //ca id = Catch Act id
+        public int dogcount; // кол-во пойманных собак
+        public int catcount; // кол-во пойманных кошек
+        public int animalcount; // кол-во пойманных животных
+        public string orgname; // название организации
+        public DateTime catchDate; // дата отлова
+        public string catchPurp; // цель отлова
+        public int applicid; // applicid = Application ID это номер заявки на отлов
+        public int mcid; // mcid = municipal contract id номер карточки муниципального контракта
+
+    }
+}
+
---
+++ Models/Organization.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shelter.Classes
+{
+    public class Organization
+    {
+        public string orgname; // наиманование организации
+        public int inn; // номер ИНН
+        public int kpp; // номер КПП
+        public string adress; // адрес организации
+        public string orgtype; // тип организации
+        public string ipyyr; // ип или юр лицо
+    }
+}
